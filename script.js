f4a430d9a89cdfd11e159d33be2c3eae4c1fd02b
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2022-08-29 11:43:28 -0300

    Trying to display books

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const bookArea = document.querySelector(".books-area");
+const addBook = document.querySelector(".add-book");
+
+addBook.addEventListener("click", addToLibrary);
+addBook.addEventListener("click", refreshBookArea);
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -9,7 +15,10 @@ function Book(title, author, pages, read) {
 
 function addToLibrary() {
   myLibrary.push(new Book("asdf", "QWER", 23, true));
-  myLibrary.push(new Book("cbvn", "CCVX", 2456, true));
-  myLibrary.push(new Book("tyiu", "MNMM", 897, false));
-  myLibrary.push(new Book("dfgh", "RETT", 11, true));
+}
+
+function refreshBookArea() {
+  for (let book of myLibrary) {
+    bookArea.innerHTML += `<div>${book.title}</div>`;
+  }
 }
