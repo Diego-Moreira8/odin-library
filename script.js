e6b94d8618812bf1dc2db1efe3af993ed9835fc5
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2022-08-31 09:57:59 -0300

    Changed refreshBookArea() to append elements

--- script.js
+++ script.js
@@ -25,11 +25,31 @@ function addToLibrary(e) {
 function refreshBookArea() {
   bookArea.innerHTML = ""; // Clear before adds
   for (let book of myLibrary) {
-    bookArea.innerHTML += `<div class="book">
-    <div class="book-title">${book.title}</div>
-    <div class="book-author">${book.author}</div>
-    <div class="book-pages">${book.pages}</div>
-    <div class="book-read">${book.read}</div>
-    </div>`;
+    // Create a new book card
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    // Create the book's details
+    const title = document.createElement("div");
+    title.classList.add("book-title");
+    title.textContent = `${book.title}`;
+
+    const author = document.createElement("div");
+    author.classList.add("book-author");
+    author.textContent = `${book.author}`;
+
+    const pages = document.createElement("div");
+    pages.classList.add("book-pages");
+    pages.textContent = `${book.pages}`;
+
+    const read = document.createElement("div");
+    read.classList.add("book-read");
+    read.textContent = `${book.read}`;
+    // Append them on the book-card
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+    // Append book-card on the book-area
+    bookArea.appendChild(bookCard);
   }
 }
