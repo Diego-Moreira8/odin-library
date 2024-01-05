c667792dd82baaccea37270c06ed1fd3ce38ec2a
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2024-01-05 16:31:47 -0300

    feat: built book and library classes

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
-const library = [];
+class Book {
+  constructor(id, title, author, totalPages, read, readPages) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.totalPages = totalPages;
+    this.read = read;
+    this.readPages = this.read ? this.totalPages : readPages;
+  }
 
-function Book() {}
+  getTitle = () => this.title;
+  getAuthor = () => this.author;
+  getTotalPages = () => this.totalPages;
+  getRead = () => this.read;
+  getReadPages = () => this.readPages;
 
-function addBookToLibrary() {}
+  setTitle = (newTitle) => (this.title = newTitle);
+
+  setAuthor = (newAuthor) => (this.author = newAuthor);
+
+  setReadPages = (newAmount) => {
+    if (newAmount >= 0 && newAmount <= this.totalPages) {
+      this.readPages = newAmount;
+    }
+    this.read = this.readPages === this.totalPages;
+  };
+
+  setTotalPages = (newAmount) => {
+    if (this.totalPages < this.readPages) {
+      this.totalPages = newAmount;
+    }
+    this.read = this.readPages === this.totalPages;
+  };
+
+  switchRead = () => {
+    this.read = !this.read;
+    if (this.read) this.readPages = this.totalPages;
+  };
+}
+
+class Library {
+  constructor() {
+    this.books = [];
+    this.id = 0;
+  }
+
+  getBooks = () => this.books;
+
+  getBook = (id) => this.books.find((book) => book.id === id);
+
+  addBook(title, author, totalPages, read, readPages) {
+    this.books.push(
+      new Book(this.id, title, author, totalPages, read, readPages)
+    );
+    this.id++;
+  }
+
+  deleteBook(id) {
+    this.books = [...this.books.filter((book) => book.id !== id)];
+  }
+}
