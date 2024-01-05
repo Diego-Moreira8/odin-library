ca526006a51f4a47933a7792e426f27f5d18c213
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2024-01-05 16:58:02 -0300

    feat: basic rendering

--- index.js
+++ index.js
@@ -59,3 +59,34 @@ class Library {
     this.books = [...this.books.filter((book) => book.id !== id)];
   }
 }
+
+class UI {
+  constructor() {
+    this.library = new Library();
+
+    this.library.addBook("a", "b", 10, true, 10);
+    this.library.addBook("b", "b", 10, false, 5);
+    this.library.addBook("c", "b", 100, true, 10);
+    this.library.addBook("d", "b", 10, true, 10);
+    this.library.addBook("e", "b", 10, false, 100);
+
+    this.update();
+  }
+
+  update() {
+    const booksList = document.querySelector("#books-list");
+    for (let book of this.library.getBooks()) {
+      const li = document.createElement("li");
+      li.textContent = `
+        ${book.getTitle()} --
+        ${book.getAuthor()} --
+        ${book.getTotalPages()} --
+        ${book.getRead()} --
+        ${book.getReadPages()} 
+      `;
+      booksList.appendChild(li);
+    }
+  }
+}
+
+new UI();
